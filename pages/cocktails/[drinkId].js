b0141c491721ce08bd40bed8dc168a25ae3c244d
J Angel Glez <a01552274@itesm.mx>
2022-09-28 18:40:10 -0500

    Cocktails drink ingredients added

--- pages/cocktails/[drinkId].js
+++ pages/cocktails/[drinkId].js
@@ -1,37 +1,35 @@
 import DrinkHeader from "../../components/drink/DrinkHeader";
+import DrinkIngredients from "../../components/drink/DrinkIngredients";
 
 const bebida = {
   title: "Sorbete de Mango 333",
   subtitle: "Reserva Espadín Joven",
   imgPrev: "img_prueba.jpg",
-  ingredientes: [
-    "Mango macerado",
-    "1 oz de jugo de limón",
-    "1 oz de jarabe de maracuyá",
-    "3/4 oz de licor de chiles",
-    "11/2 oz de Mezcal Reserva Espadín",
+  ingredients: [
+    { ing: "Mango macerado", imgP: "mango.png" },
+    { ing: "1 oz de jugo de limón", imgP: "mango.png" },
+    { ing: "1 oz de jarabe de maracuyá", imgP: "mango.png" },
+    { ing: "3/4 oz de licor de chiles", imgP: "mango.png" },
+    { ing: "11/2 oz de Mezcal Reserva Espadín", imgP: "mango.png" },
   ],
-  imgsIngredientes: [
-    "mango.jpg",
-    "limon.jpg",
-    "maracuya.jpg",
-    "chiles.jpg",
-    "mezcal.jpg",
-  ],
-  pasos: [
+  steps: [
     "Colocar todos los ingredientes en una licuadora hasta que se deshaga el mango.",
     "Colocar todos los ingredientes en una licuadora hasta que se deshaga el mango.",
     "Escarchar la copa con chile piquín y decorar con un trozo de mango.",
   ],
 };
 
+// I will change bebida.title for props.title
 const DrinkPage = () => {
   return (
-    <DrinkHeader
-      title={bebida.title}
-      subtitle={bebida.subtitle}
-      imgPrev={bebida.imgPrev}
-    />
+    <>
+      <DrinkHeader
+        title={bebida.title}
+        subtitle={bebida.subtitle}
+        imgPrev={bebida.imgPrev}
+      />
+      <DrinkIngredients ingredients={bebida.ingredients} />
+    </>
   );
 };
 
