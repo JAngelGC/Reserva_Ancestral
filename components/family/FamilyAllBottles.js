5326c4c18f31c7b54284bfb485cac14d8341ee63
J Angel Glez <a01552274@itesm.mx>
2022-09-29 14:28:41 -0500

    Fetching all bottles preview from firestore

--- components/family/FamilyAllBottles.js
+++ components/family/FamilyAllBottles.js
@@ -1,41 +1,10 @@
 import FamillyBottlePrev from "./FamillyBottlePrev";
 import classes from "./FamilyAllBottles.module.css";
 
-const ALL_BOTTLES = [
-  {
-    title: "Reserva Arroqueño",
-    subtitle: "Agave Arroqueño",
-    grades: "Grados de alcohol: 45°",
-    nameImage: "botella_1.png",
-    color: "arroqueno",
-  },
-  {
-    title: "Reserva Cuixe",
-    subtitle: "Agave Cuixe",
-    grades: "Grados de alcohol: 45°",
-    nameImage: "botella_3.png",
-    color: "cuixe",
-  },
-  {
-    title: "Reserva Espadín",
-    subtitle: "Agave Espadín",
-    grades: "Grados de alcohol: 45°",
-    nameImage: "botella_2.png",
-    color: "espadin",
-  },
-  {
-    title: "Reserva Tobalá",
-    subtitle: "Agave Tobalá",
-    grades: "Grados de alcohol: 45°",
-    nameImage: "botella_4.png",
-    color: "tobala",
-  },
-];
-
-const FamilyAllBottles = () => {
+const FamilyAllBottles = (props) => {
   return (
     <>
-      {ALL_BOTTLES.map((bottle) => {
+      {props.allBottles.map((bottle) => {
         return (
           <FamillyBottlePrev
             key={bottle.title}
--- pages/family/index.js
+++ pages/family/index.js
@@ -1,7 +1,30 @@
+import { collection, getDocs } from "firebase/firestore";
+import db from "../../firebase/config";
+
 import FamilyAllBottles from "../../components/family/FamilyAllBottles";
 
-const Family = () => {
-  return <FamilyAllBottles />;
+const Family = (props) => {
+  return <FamilyAllBottles allBottles={props.allBottles} />;
 };
+
+export async function getStaticProps() {
+  // fetch data from an API
+  const colRef = collection(db, "bottlesPreview");
+  const snapshots = await getDocs(colRef);
+
+  const docs = snapshots.docs.map((doc) => {
+    const data = doc.data();
+    data.id = doc.id;
+
+    return data;
+  });
+
+  return {
+    props: {
+      allBottles: docs,
+    },
+    revalidate: 3600 * 24,
+  };
+}
 
 export default Family;
