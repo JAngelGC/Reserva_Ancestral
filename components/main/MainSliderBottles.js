6daca4fe549ad3b42fc06eef272cc87673c09359
J Angel Glez <a01552274@itesm.mx>
2022-09-29 14:52:16 -0500

    Fetching bottles preview from firestore

--- components/main/MainSliderBottles.js
+++ components/main/MainSliderBottles.js
@@ -15,11 +15,11 @@ import { EffectCoverflow, Pagination } from "swiper";
 // import SlideBottle from "./SlideBottle";
 import classes from "./MainSliderBottles.module.css";
 
-const MainSliderBottles = () => {
+const MainSliderBottles = (props) => {
+  console.log(props);
   return (
     <section className={classes["section-slider--bottles"]}>
       <h2>Nuestra familia reserva</h2>
-      {/* <SlideBottle /> */}
 
       <Swiper
         effect={"coverflow"}
@@ -44,86 +44,32 @@ const MainSliderBottles = () => {
         modules={[EffectCoverflow, Pagination]}
         className={`${classes["my-swiper"]}`}
       >
-        <SwiperSlide
-          className={`${classes["my-swiper--slide"]} ${classes.arroqueno}`}
-        >
-          {({ isActive }) => (
-            <div
-              className={`${classes["container-slide"]} ${
-                isActive ? classes.active : classes.noactive
+        {props.allBottles.map((bottle) => {
+          return (
+            <SwiperSlide
+              className={`${classes["my-swiper--slide"]} ${
+                classes[bottle.color]
               }`}
             >
-              <img
-                src="./static/images/botella_1.png"
-                alt=""
-                className={classes["img-bottle"]}
-              />
-              <h3>Reserva Arroqueño</h3>
-              <h4>Arroqueño</h4>
-              <button className={classes.arroqueno}>Descúbrelo</button>
-            </div>
-          )}
-        </SwiperSlide>
-        <SwiperSlide
-          className={`${classes["my-swiper--slide"]} ${classes.espadin}`}
-        >
-          {({ isActive }) => (
-            <div
-              className={`${classes["container-slide"]} ${
-                isActive ? classes.active : classes.noactive
-              }`}
-            >
-              <img
-                src="./static/images/botella_2.png"
-                alt=""
-                className={classes["img-bottle"]}
-              />
-              <h3>Reserva Espadín</h3>
-              <h4>Espadín</h4>
-              <button className={classes.espadin}>Descúbrelo</button>
-            </div>
-          )}
-        </SwiperSlide>
-        <SwiperSlide
-          className={`${classes["my-swiper--slide"]} ${classes.cuixe}`}
-        >
-          {({ isActive }) => (
-            <div
-              className={`${classes["container-slide"]} ${
-                isActive ? classes.active : classes.noactive
-              }`}
-            >
-              <img
-                src="./static/images/botella_3.png"
-                alt=""
-                className={classes["img-bottle"]}
-              />
-              <h3>Reserva Cuixe</h3>
-              <h4>Cuixe</h4>
-              <button className={classes.cuixe}>Descúbrelo</button>
-            </div>
-          )}
-        </SwiperSlide>
-        <SwiperSlide
-          className={`${classes["my-swiper--slide"]} ${classes.tobala}`}
-        >
-          {({ isActive }) => (
-            <div
-              className={`${classes["container-slide"]} ${
-                isActive ? classes.active : classes.noactive
-              }`}
-            >
-              <img
-                src="./static/images/botella_4.png"
-                alt=""
-                className={classes["img-bottle"]}
-              />
-              <h3>Reserva Tobalá</h3>
-              <h4>Tobalá</h4>
-              <button className={classes.tobala}>Descúbrelo</button>
-            </div>
-          )}
-        </SwiperSlide>
+              {({ isActive }) => (
+                <div
+                  className={`${classes["container-slide"]} ${
+                    isActive ? classes.active : classes.noactive
+                  }`}
+                >
+                  <img
+                    src={`./static/images/${bottle.nameImage}`}
+                    alt=""
+                    className={classes["img-bottle"]}
+                  />
+                  <h3>{bottle.title}</h3>
+                  <h4>{bottle.subtitle}</h4>
+                  <button className={classes[bottle.color]}>Descúbrelo</button>
+                </div>
+              )}
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </section>
   );
