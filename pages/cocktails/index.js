70736a2fc61e0d390a0ca69d8ebc97871a5c6dc0
J Angel Glez <a01552274@itesm.mx>
2022-09-29 18:45:49 -0500

    getStatticProps updated to improve reusability

--- pages/cocktails/index.js
+++ pages/cocktails/index.js
@@ -9,14 +9,19 @@ const CocktailsPage = (props) => {
 
 export async function getStaticProps() {
   // fetch data from an API
-  const colRef = collection(db, "drinksPreview");
+  const colRef = collection(db, "drinks");
   const snapshots = await getDocs(colRef);
 
   const docs = snapshots.docs.map((doc) => {
     const data = doc.data();
-    data.id = doc.id;
 
-    return data;
+    const newData = {
+      id: doc.id,
+      imgPrev: data.imgPrev,
+      title: data.title,
+    };
+
+    return newData;
   });
 
   return {
