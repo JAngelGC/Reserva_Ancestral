c853d1f1227013b1c7a976d9714b182f5a1f4198
J Angel Glez <a01552274@itesm.mx>
2022-10-07 12:21:55 -0500

    Responsive design for home page implemented

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -4,15 +4,16 @@ import classes from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={classes.footer}>
-      <div className={classes.up}>
+      <div className={classes["container-text"]}>
         <Link href="/contact">
           <h3 className={classes.contacto}>Contacto</h3>
         </Link>
         <h3 className={classes.aviso}>Aviso de privacidad</h3>
+        <p className={classes.derechos}>
+          © 2022 Reserva Ancestral, Derechos reservados.
+        </p>
       </div>
-      <p className={classes.derechos}>
-        © 2022 Reserva Ancestral, Derechos reservados.
-      </p>
+
       <img src="/static/images/Sello_Reserva_Ancestral.png" alt="" />
     </footer>
   );
