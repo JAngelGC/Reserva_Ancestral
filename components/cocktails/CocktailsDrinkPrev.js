6670ed486375aa23d8bb02f768c521cec54d9b51
J Angel Glez <a01552274@itesm.mx>
2022-10-25 13:30:11 -0500

    SrcSet and sizes for images

--- components/cocktails/CocktailsDrinkPrev.js
+++ components/cocktails/CocktailsDrinkPrev.js
@@ -1,28 +1,22 @@
 import Link from "next/link";
-import useMediaQuery from "../../hooks/use-window-dimensions";
 
 import classes from "./CocktailsDrinkPrev.module.css";
 
 const CocktailsDrinkPrev = (props) => {
-  // const isBreakpoint = useMediaQuery(544);
-
-  // const selectedImage = isBreakpoint ? props.imgPrevPhone : props.imgPrev;
-  // console.log(selectedImage);
+  console.log(props.imgPrev);
 
   return (
     <Link href={`/cocktails/${props.id}`}>
       <section className={classes["container-drink"]}>
         <div className={classes["container-img"]}>
           <img
-            src={`./static/images/drinks/${selectedImage}`}
-            // sizes="(max-width: 1400px) 100vw, 1400px"
-            srcset={`
-          ${props.imgPrev},w_200.jpg 200w,
-          ${props.imgPrev},w_607.jpg 607w,
-          ${props.imgPrev},w_879.jpg 879w,
-          ${props.imgPrev},w_1122.jpg 1122w,
-          ${props.imgPrev},w_1347.jpg 1347w,
-          ${props.imgPrev},w_1400.jpg 1400w`}
+            sizes="(min-width: 1366px) 916px,
+            (min-width: 1536px) 1030px,
+            100vw"
+            srcSet={`
+          ${`./static/images/drinks/${props.imgPrev}_607.jpg`} 607w,
+          ${`./static/images/drinks/${props.imgPrev}_879.jpg`} 879w,
+          ${`./static/images/drinks/${props.imgPrev}_1400.jpg`} 1400w`}
             alt=""
           />
         </div>
