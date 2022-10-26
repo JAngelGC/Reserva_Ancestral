4040acffaa22041ec904a6c8e793ac64528c4d0f
J Angel Glez <a01552274@itesm.mx>
2022-10-26 12:24:52 -0500

    Updating pictures for bottles

--- components/family/FamillyBottlePrev.js
+++ components/family/FamillyBottlePrev.js
@@ -10,7 +10,25 @@ const FamillyBottlePrev = (props) => {
         <button className={classes[props.color]}>Descúbrelo</button>
       </div>
       <div className={classes["container-img"]}>
-        <img src={`./static/images/${props.nameImage}`} alt="" />
+        {/* <img src={`./static/images/${props.nameImage}`} alt="" /> */}
+        <picture>
+          <source
+            srcSet={`./static/images/${props.nameImage}_607.png`}
+            media="(max-width:544px)"
+          />
+          <source
+            srcSet={`./static/images/${props.nameImage}_879.png`}
+            media="(max-width:940px)"
+          />
+          <source
+            srcSet={`./static/images/${props.nameImage}_1400.png`}
+            media="(max-width:1400px)"
+          />
+          <img
+            src={`./static/images/${props.nameImage}_1400.png`}
+            alt="Gabrielle Perfume"
+          />
+        </picture>
       </div>
     </section>
   );
