cbfdfff3cc9a9b4431a4c102f8df48152dd70a32
J Angel Glez <a01552274@itesm.mx>
2022-10-26 23:49:34 -0500

    Using react-youtube

--- components/drink/DrinkPreparation.js
+++ components/drink/DrinkPreparation.js
@@ -1,16 +1,34 @@
+import React from "react";
+import YouTube from "react-youtube";
 import classes from "./DrinkPreparation.module.css";
 
 const DrinkPreparation = (props) => {
+  const opts = {
+    height: "500",
+    width: "640",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
   return (
     <section className={classes["section-preparation"]}>
       <div className={classes["container-video"]}>
-        <iframe
+        {/* <iframe
           src={`https://www.youtube.com/embed/${props.embedId}`}
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
           title="Embedded youtube"
-        />
+        /> */}
+        <div className={classes["vid-cont"]}>
+          <YouTube
+            className={classes["video"]}
+            videoId={props.embedId}
+            opts={opts}
+            onReady={YouTube.onReady}
+          />
+        </div>
       </div>
 
       <div className={classes["container-steps"]}>
