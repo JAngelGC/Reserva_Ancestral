5bc0d167d20bf15a66702c940f9821336a75683e
J Angel Glez <a01552274@itesm.mx>
2022-10-30 18:27:31 -0600

    Updating .gitignore

---
+++ firebase/config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_KEY,
+  authDomain: "reservaancestral-96c1b.firebaseapp.com",
+  projectId: "reservaancestral-96c1b",
+  storageBucket: "reservaancestral-96c1b.appspot.com",
+  messagingSenderId: "412066224523",
+  appId: "1:412066224523:web:cc8a8d6d236d225266378e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export default db;
