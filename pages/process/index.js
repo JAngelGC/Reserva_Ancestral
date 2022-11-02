6a86c11135ff7b4e943b8cd7efc5b080d7976994
J Angel Glez <a01552274@itesm.mx>
2022-11-02 01:45:52 -0600

    Adding head to process page

--- pages/process/index.js
+++ pages/process/index.js
@@ -1,8 +1,22 @@
+import Head from "next/head";
 import AllProcess from "../../components/process/AllProcess";
 
 const ProcessPage = () => {
   return (
     <>
+      <Head>
+        <title>Reserva Ancestral</title>
+        <meta
+          name="description"
+          content="Reserva Ancestral nace de la admiración, talento y honra a todos aquellos maestros artesanos que desde hace cientos de años, conservan la tradición de crear una bebida única a través del aprendizaje y dominio de un proceso milenario: el destilado ancestral."
+        />
+        <meta name="author" content="Jose Angel Gonzalez Carrera" />
+        <link
+          rel="icon"
+          href="/static/images/logos/Reserva_Ancestral_Icon.png"
+          type="image/x-icon"
+        />
+      </Head>
       <AllProcess />
     </>
   );
